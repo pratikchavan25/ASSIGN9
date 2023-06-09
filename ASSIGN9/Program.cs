7941feff0bf2582a3b6baf3d0d72f2506a42ca50
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-09 14:21:39 +0530

    Add project files.

---
+++ ASSIGN9/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 9. Write a  program to enter temperature in Fahrenheit(°F) and convert it into Celsius(°C) ?
+
+double celsius, fahrenheit;
+
+Console.WriteLine("enter the temperature in fahrenheit");
+
+fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+celsius = (fahrenheit - 32) * 0.55;
+
+Console.WriteLine(celsius);
